c5146518ce6ec5d0ae7d07a70a156d3a214de369
Aston Varghese <astonvarghese8@gmail.com>
2025-01-21 13:55:39 -0500

    installed some dependencies

--- crud-frontend/src/App.jsx
+++ crud-frontend/src/App.jsx
@@ -4,11 +4,11 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1>Hello World</h1>
+      <h1 className='text-3xl font-bold underline'>
+        Hello World!
+        </h1>
     </>
   )
 }
