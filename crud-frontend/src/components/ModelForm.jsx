fb15148ab0a8227e0d81464bac98a2dab83b15b6
Aston Varghese <astonvarghese8@gmail.com>
2025-01-28 13:46:19 -0500

    Update ModelForm.jsx
    updated add client/update client page

--- crud-frontend/src/components/ModelForm.jsx
+++ crud-frontend/src/components/ModelForm.jsx
@@ -7,6 +7,32 @@ export default function ModelForm({isOpen, onClose, mode, OnSubmit}) {
             <div className="modal-box">
                 <form method="dialog">
                 {/* if there is a button in form, it will close the modal */}
+
+                <label className="input input-bordered my-4 flex items-center gap-2">
+                Name
+                    <input type="text" className="grow" />
+                </label>
+                <label className="input input-bordered my-4 flex items-center gap-2">
+                Email
+                    <input type="text" className="grow" />
+                </label>
+                <label className="input input-bordered my-4 flex items-center gap-2">
+                Job
+                    <input type="text" className="grow" />
+                </label>
+
+                <div className="flex mb-4 justify-between my-4">
+                    <label className="input input-bordered mr-4 flex items-center gap-2">
+                    Rate
+                        <input type="number" className="grow" />
+                    </label> 
+
+                    <select className="select select-bordered mt-4 w-full max-w-xs">
+                        <option>Inactive</option>
+                        <option>Active</option>
+                    </select> 
+                </div>
+
                 <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
                 <button className="btn btn-success">{mode==='edit'? 'Save Chnages' : 'Add Client'}</button>
                 </form>
