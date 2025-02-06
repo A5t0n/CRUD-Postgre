4e66cb01d4dc31b97c28ba72b153f670e5dc292f
Aston Varghese <astonvarghese8@gmail.com>
2025-02-06 14:22:37 -0500

    databse connection initlaization

--- crud-backend/src/index.js
+++ crud-backend/src/index.js
@@ -1,12 +1,15 @@
 import express from "express";
+import cors from "cors";
+import clientRoutes from "./routes/clientRoute.js";
 
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send ('<h1>Hello backend</h1>');
-});
+app.use(cors());
+app.use(express.json());
+
+app.use("/api", clientRoutes);
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
